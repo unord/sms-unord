c537006629f981600657747ed5dfa24deebd9b11
Reventlow <gorm@reventlow.com>
2023-01-09 12:54:51 +0100

    Dockerfile update

--- sms_app/admin.py
+++ sms_app/admin.py
@@ -24,12 +24,32 @@ class MessageAdmin(admin.ModelAdmin):
         "validated_by_email",
     ]
     readonly_fields = [
-        "last_updated",
-        "completed",
-        "message",
-        "email",
-        "link_code",
-        "time_to_send",
-        "created",
-        "validated_by_email",
+
     ]
+
+
+class RecipientAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = models.Recipient
+        fields = "__all__"
+
+
+class RecipientAdmin(admin.ModelAdmin):
+    form = RecipientAdminForm
+    list_display = [
+        "created",
+        "last_name",
+        "mobile_number",
+        "sent",
+        "last_updated",
+        "first_name",
+        "message",
+    ]
+    readonly_fields = [
+
+    ]
+
+
+admin.site.register(models.Message, MessageAdmin)
+admin.site.register(models.Recipient, RecipientAdmin)
