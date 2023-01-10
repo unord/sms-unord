1f0cb47e8ea77ed6d2904c0624a0af915d35f3d6
Reventlow <gorm@reventlow.com>
2023-01-10 09:38:17 +0100

    settings to danish

--- MessageSys/settings.py
+++ MessageSys/settings.py
@@ -13,6 +13,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
