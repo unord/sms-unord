a7aed14d0c96a2da73edeca818050d89dd47cdb6
Reventlow <gorm@reventlow.com>
2023-01-10 09:00:46 +0100

    settings to danish

--- MessageSys/settings.py
+++ MessageSys/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'da-dk'
+LANGUAGE_CODE = 'da'
 
 TIME_ZONE = 'Europe/Copenhagen'
 
