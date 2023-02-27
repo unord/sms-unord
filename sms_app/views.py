a8681c67ba912b4d55bd3a3fea3033a86f40a227
Gorm Reventlow <gorm@reventlow.com>
2023-02-27 12:26:48 +0100

    upload excel str(mobile number)

--- sms_app/views.py
+++ sms_app/views.py
@@ -192,7 +192,7 @@ def import_data(request):
                 try:
                     mobile_error.append(f"Fejl i telefon nummer række: {request.POST['inputGroupSelectMobile']}{row_count}. Værdi: {org_value.replace('<NA>', 'Tom felt')}")
                 except:
-                    mobile_error.append(f"Fejl i telefon nummer række: {row_count}.")
+                    mobile_error.append(f"Fejl i telefon nummer række: {request.POST['inputGroupSelectMobile']}{row_count}.")
 
         if len(mobile_error) > 0:
             print(df_to_analyze)
